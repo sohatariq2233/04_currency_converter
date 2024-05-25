f1c5d3c1865a3e4c53a8070729385553e2d398f8
sohatariq2233 <sohatariq2233@gmail.com>
2024-05-25 12:05:11 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer"
+
+let exchange_rate: any = {
+    "USD":1,
+    "EUR":0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 277,
+
+}
+
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select the currency to convert from:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select the currency to convert into:",
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    { 
+        name: "amount",
+        type: "input",
+        message: "Enter the amount to convert: "
+    }
+])
+
+let from_amount = exchange_rate[userAnswer.from_currency]
+let to_amount = exchange_rate[userAnswer.to_currency]
+let amount = userAnswer.amount
+
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+console.log(`Converted Amount = ${converted_amount}`)
